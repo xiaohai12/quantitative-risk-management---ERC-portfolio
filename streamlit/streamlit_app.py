570e6344875078b52ea3ed316206044238b49407
shengzhao <shengzhaolei@gmail.com>
2025-11-23 22:02:48 +0100

    change name

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -7,11 +7,11 @@ ERC_portfolio_page = st.Page("ERC_portfolio.py", title="ERC portfolio")
 Contact_page = st.Page("Contact.py", title="Contact US")
 Risk_Preference = st.Page("risk_preference.py", title="Risk Preference")
 Our_Team = st.Page("team.py", title="Our Team")
-ERC_strategy_information = st.Page("Strategy_information.py", title="ERC Strategy Information")
+Methodology = st.Page("methodology.py", title="ERC Strategy Information")
 LLM = st.Page("LLM.py", title="LLM")
 
 # Set up navigation
-pg = st.navigation([home_page, ERC_portfolio_page, Risk_Preference, Our_Team, Contact_page, ERC_strategy_information, LLM])
+pg = st.navigation([home_page, ERC_portfolio_page, Risk_Preference, Our_Team, Contact_page, Methodology, LLM])
 
 # Run the selected page
 pg.run()
