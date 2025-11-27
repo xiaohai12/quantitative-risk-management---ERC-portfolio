560fcabab3b5d871b0fc5a82d5c9fc2f2a0a167b
shengzhao <shengzhaolei@gmail.com>
2025-11-27 18:55:23 +0100

    add img-dit

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -3,7 +3,7 @@ from streamlit import title
 
 # Define the pages
 home_page = st.Page("home_page.py", title="Home Page")
-ERC_portfolio_page = st.Page("ERC_portfolio.py", title="ERC portfolio")
+
 Contact_page = st.Page("Contact.py", title="Contact US")
 Risk_Preference = st.Page("risk_preference.py", title="Risk Preference")
 Our_Team = st.Page("team.py", title="Our Team")
@@ -11,7 +11,7 @@ Methodology = st.Page("methodology.py", title="Methodology")
 LLM = st.Page("LLM.py", title="LLM")
 
 # Set up navigation
-pg = st.navigation([home_page, ERC_portfolio_page, Risk_Preference, Our_Team, Contact_page, Methodology, LLM])
+pg = st.navigation([home_page, Risk_Preference, ERC_portfolio_page, Our_Team, Contact_page, Methodology, LLM])
 
 # Run the selected page
 pg.run()
